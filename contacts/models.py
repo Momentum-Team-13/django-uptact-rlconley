32cb2d17184b53914e241a998b8d7a9f57a1967f
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2022-06-28 11:30:08 -0400

    add favorite model

--- contacts/models.py
+++ contacts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.validators import RegexValidator
 from localflavor.us.models import USStateField, USZipCodeField
-
+from users.models import User
 
 class Contact(models.Model):
     phone_regex = RegexValidator(
@@ -10,6 +10,8 @@ class Contact(models.Model):
 
     name = models.CharField(max_length=255)
     email = models.EmailField(null=True, blank=True)
+    birthday = models.DateField(blank=True, null=True)
+    # order doesn'e matter with keyword arguments
     phone_number = models.CharField(max_length=11,
                                     validators=[phone_regex],
                                     null=True,
@@ -19,3 +21,8 @@ class Contact(models.Model):
     city = models.CharField(max_length=255, null=True, blank=True)
     state = USStateField(null=True, blank=True)
     zip_code = USZipCodeField(null=True, blank=True)
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="favorites")
+    contact = models.ForeignKey(Contact,on_delete=models.CASCADE, related_name="favorites")
